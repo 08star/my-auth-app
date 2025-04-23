9a415c0a0705f3fb917db719e14e7f2ed1c7c7bd
08star <qwer221082@gmail.com>
2025-04-23 16:57:56 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
+# /home/deploy/my-auth-app/wsgi.py
 import os, sys
+# 确保项目根目录在 path 里
 sys.path.insert(0, os.path.dirname(__file__))
 
 from app import app
 
 if __name__ == "__main__":
+    # 仅作本地调试用
     app.run(host="127.0.0.1", port=8000)
