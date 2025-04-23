be182fd0ec9a3342b9e4d8a1ba008193042554cd
08star <qwer221082@gmail.com>
2025-04-23 16:54:45 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,7 @@
-# wsgi.py
 import os, sys
-# 确保当前目录在 Python 路径里
 sys.path.insert(0, os.path.dirname(__file__))
 
 from app import app
 
-# Gunicorn 会导入这个 app
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host="127.0.0.1", port=8000)
